9bad30f5765e790af0bfe5346088809c9e7afbf1
Raviprasad666666 <31849332+Raviprasad666666@users.noreply.github.com>
2017-09-12 01:40:04 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	Student st=new Student();
+		st.setId("15215");
+		st.setFullName("ravi");
+		st.setBirthDate("04-05-1993");
+		st.setAvgMark(73.333);
+		
+	}
+
+}
